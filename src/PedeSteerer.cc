a3356707686faa02621c52275fa158cfbd195f0d
cmsbuild <>
2008-03-25 16:15:59 +0000

    This commit was manufactured by cvs2git to create tag 'Alignment- MillePedeAlignmentAlgorithm-V00-14-03_forCMSSW17X'.
    Sprout from master 2008-03-25 16:15:57 UTC Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch> 'reactivate monitoring of derivatives (and remove debug message in MillePedeAlignmentAlgorithm.cc)'
    Cherrypick from master 2007-03-16 17:03:02 UTC Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch> 'write correction parameters as millepede user variables (normal parameters tree now contains remaining misalignment)':
        src/MillePedeVariablesIORoot.cc
    Cherrypick from master 2008-02-27 08:01:15 UTC Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch> 'backport V00-14-00 to CMSSW_1_7_X without new tracker hierarchy':
        src/PedeSteerer.cc

--- src/PedeSteerer.cc
+++ src/PedeSteerer.cc
@@ -314,7 +314,8 @@ void PedeSteerer::correctToReferenceSystem()
   std::vector<Alignable*> definerDets; // or ...DetUnits
   for (std::vector<Alignable*>::iterator it = theCoordDefiners.begin(), iE = theCoordDefiners.end();
        it != iE; ++it) {// find lowest level objects of alignables that define the coordinate system
-    const std::vector<Alignable*> &comp = (*it)->deepComponents();
+    std::vector<Alignable*> comp;
+    (*it)->deepComponents(comp);
     definerDets.insert(definerDets.end(), comp.begin(), comp.end());
   }
 
@@ -449,7 +450,7 @@ void PedeSteerer::hierarchyConstraint(const Alignable *ali,
 	file << "\n* Nr. " << iConstr << " of a '"
 	     << objId.typeToName(ali->alignableObjectId()) << "' (label "
 	     << myLabels->alignableLabel(const_cast<Alignable*>(ali)) // ugly cast: FIXME!
-	     << "), layer " << aliId.typeAndLayerFromDetId(ali->id()).second
+	     << "), layer " << aliId.typeAndLayerFromAlignable(ali).second //DetId(ali->id()).second
 	     << ", position " << ali->globalPosition()
 	     << ", r = " << ali->globalPosition().perp();
       }
